1fb7873602ae1ccdcf56da08e2fa5c5761117dbe
xhsdf <xhsdf.misc@googlemail.com>
2016-07-09 20:48:51 +0200

    now skips other subtitles with same language, fixed problem with single quote in file names

--- extract.rb
+++ extract.rb
@@ -18,14 +18,14 @@ def run(file, series, output_main_dir, clean)
 	
 	get_subtitle_tracks(file, clean).each do |index, lang|
 		puts "  extracting subtitle track: #{lang}..."
-		subtitle_dir = "#{output_main_dir}/#{series}/#{filename}/#{lang}"
+		subtitle_dir = "#{output_main_dir}/#{series}/#{filename}/#{lang}".gsub("'", "´")
 		FileUtils.mkdir_p subtitle_dir
 		sub_param = ""
 		if lang != 'none'
 			system("ffmpeg -i \"#{file}\" -loglevel error -y -map 0:#{index} \"#{subtitle_dir}.ass\"")
 			sub_param = ", ass='#{subtitle_dir}.ass'"
 		end
-		puts "  extracting screencaps with subtitle track: #{lang}..."
+		puts "  extracting screencaps with subtitle track: #{lang}..."		
 		system("ffmpeg -i \"#{file}\" -loglevel error -start_number 0 -stats -vf \"scale=-1:480, fps=1#{sub_param}\" -q:v 1 \"#{subtitle_dir}\"/%d.jpg")
 	end
 end
@@ -41,7 +41,7 @@ def get_subtitle_tracks(file, clean = true)
 		XPath.each(stream, "tag[@key = 'language']/@value") do |lang_attr|
 			lang = lang_attr
 		end
-		tracks[index] = lang
+		tracks[index] = lang unless tracks.values.include? lang
 	end
 	tracks['none'] = 'none' if clean
 	return tracks
